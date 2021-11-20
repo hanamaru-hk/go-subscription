9b83f362f6b9745fd180c7f463761b0240bfc473
pakyinwww <pakyinwww@gmail.com>
2021-11-21 04:24:49 +0800

    feat(header): add 'allows cors' header

--- main.go
+++ main.go
@@ -7,6 +7,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		c.Next()
+	}
+}
+
 func handleList(c *gin.Context) {
 	var loadedEmails, _ = GetAll()
 	var result string
@@ -36,6 +53,7 @@ func handleSubscribe(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
+	r.Use(Cors())
 	r.GET("/list/", handleList)
 	r.POST("/subscribe/", handleSubscribe)
 	r.Run(goDotEnvVariable("PORT")) // listen and serve on 0.0.0.0:4001
